63951e831da4cd0878959a08c8c91d082e05d81b
Lambodar Bag <152137585+lambodarbag@users.noreply.github.com>
2024-03-10 14:57:09 +0530

    Feats: Updated

---
+++ GuessTheNumber/script.js
@@ -0,0 +1,7 @@
+const randomNumber = parseInt(Math.random()*100+1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".gusseses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
